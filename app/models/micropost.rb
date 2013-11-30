f4aee3c81e3507f692e8195f4c51b60982d54083
Josh Kalov <jkalov@gmail.com>
2013-11-30 01:30:35 -0600

    removed ipc constraint

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,7 +2,7 @@ class Micropost < ActiveRecord::Base
   belongs_to :user
 
   validates :user_id, presence: true
-  validates :ipc, presence: true
+  validates :ipc
   validates :content, presence: true, length: { maximum: 140 }
 
   default_scope -> { order('microposts.created_at DESC') }
