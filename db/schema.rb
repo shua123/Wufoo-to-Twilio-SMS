00592181a6a51baaaf2a95e0c0f21da92cdd6b29
Josh Kalov <jkalov@gmail.com>
2013-11-29 22:54:10 -0600

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20120308215846) do
+ActiveRecord::Schema.define(version: 20131130043438) do
 
   create_table "microposts", force: true do |t|
     t.string   "content"
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "ipc"
   end
 
   add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
