c70155ecca985ab7428cf1cde9f5ea865acf7abc
Josh Kalov <jkalov@gmail.com>
2013-12-01 00:32:24 -0600

    Make feed show all messages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,8 @@ class StaticPagesController < ApplicationController
   def home
     if signed_in?
       @micropost  = current_user.microposts.build
-      @feed_items = current_user.feed.paginate(page: params[:page])
+      #@feed_items = current_user.feed.paginate(page: params[:page])
+      @feed_items = Micropost.paginate(page: params[:page])
     end
   end
 
