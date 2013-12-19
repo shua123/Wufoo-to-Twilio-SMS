ce24c318e81433960379068d8c87907e57ca9574
Josh Kalov <jkalov@gmail.com>
2013-12-18 18:15:47 -0600

    Fix char. countdown bug. Add navbar active state

--- app/assets/javascripts/character_countdown.js
+++ app/assets/javascripts/character_countdown.js
@@ -1,19 +1,22 @@
 function updateCountdown() {
-  // 140 characters max
-  var left = 160 - jQuery('#micropost_content').val().length;
-  if(left == 1) {
-    var charactersLeft = ' character left.'
+  if ($('#micropost_content').length){
+    // 160 characters max
+
+    var left = 160 - jQuery('#micropost_content').val().length;
+    if(left == 1) {
+      var charactersLeft = ' character left.'
+    }
+    else if (left == -1){
+      var charactersLeft = ' character too many.'
+    }
+    else if(left < -1){
+      var charactersLeft = ' characters too many.'
+    }
+    else{
+      var charactersLeft = ' characters left.'
+    }
+    jQuery('.countdown').text(Math.abs(left) + charactersLeft);
   }
-  else if (left == -1){
-    var charactersLeft = ' character too many.'
-  }
-  else if(left < -1){
-    var charactersLeft = ' characters too many.'
-  }
-  else{
-    var charactersLeft = ' characters left.'
-  }
-  jQuery('.countdown').text(Math.abs(left) + charactersLeft);
 }
 
 jQuery(document).ready(function($) {
