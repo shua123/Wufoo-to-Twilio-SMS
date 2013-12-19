2b3eb93c31b924a43ce048ac0918f2bf38f0112c
Josh Kalov <jkalov@gmail.com>
2013-12-19 11:06:35 -0600

    Update language for new user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,9 +20,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      # sign_in @user
+      flash[:success] = "New User Created!"
+      redirect_to (root_url)
     else
       render 'new'
     end
