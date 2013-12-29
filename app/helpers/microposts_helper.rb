b01e20f085c605c6dc0dfb4f5475f9daa6c84daf
Josh Kalov <jkalov@gmail.com>
2013-12-29 14:57:58 -0600

    Improve twilio error flash

--- app/helpers/microposts_helper.rb
+++ app/helpers/microposts_helper.rb
@@ -3,19 +3,19 @@ module MicropostsHelper
   def messagesSent
     begin
         client = Twilio::REST::Client.new(ENV['TWILIO_SID'], ENV['TWILIO_TOKEN']) 
-        list = client.account.messages.list({:from => ENV['TWILIO_FROM'], :page_size => 200})
-    rescue
-        flash[:error] =  "There was a problem loading sent messages from Twilio."
-        list = []   
+        list = client.account.messages.list({:from => ENV['TWILIO_FROM'], :page_size => 400})
+    rescue => error
+        flash[:error] =  "There was a problem loading sent messages from Twilio. Error message: #{error.message}"
+        list = [] 
     end
   end
 
   def messagesReceived
     begin
         client = Twilio::REST::Client.new(ENV['TWILIO_SID'], ENV['TWILIO_TOKEN']) 
-        listreceived = client.account.messages.list({:to => ENV['TWILIO_FROM'], :page_size => 200})
+        listreceived = client.account.messages.list({:to => ENV['TWILIO_FROM'], :page_size => 400})
     rescue
-        flash[:error] =  "There was a problem loading received messages from Twilio."   
+        flash[:error] =  "There was a problem loading received messages from Twilio. Error message: #{error.message}"   
         list = []
     end
   end
