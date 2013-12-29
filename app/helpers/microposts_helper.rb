fb4e8f2574f17c9ab6503af8d8c9f0cd8849a81e
Josh Kalov <jkalov@gmail.com>
2013-12-29 04:35:59 -0600

    down to last 200 messages

--- app/helpers/microposts_helper.rb
+++ app/helpers/microposts_helper.rb
@@ -2,13 +2,13 @@ module MicropostsHelper
   
   def messagesSent
     client = Twilio::REST::Client.new(ENV['TWILIO_SID'], ENV['TWILIO_TOKEN']) 
-    list = client.account.messages.list({:from => ENV['TWILIO_FROM'], :page_size => 1000})
+    list = client.account.messages.list({:from => ENV['TWILIO_FROM'], :page_size => 200})
     listtemp = list
   end
 
   def messagesReceived
     client = Twilio::REST::Client.new(ENV['TWILIO_SID'], ENV['TWILIO_TOKEN']) 
-    listreceived = client.account.messages.list({:to => ENV['TWILIO_FROM'], :page_size => 1000})
+    listreceived = client.account.messages.list({:to => ENV['TWILIO_FROM'], :page_size => 200})
   end
   
 end
