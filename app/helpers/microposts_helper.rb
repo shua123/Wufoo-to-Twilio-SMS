abc6ce439f708714a83a5f4aeb1d2637f6f780f0
Josh Kalov <jkalov@gmail.com>
2013-12-29 13:10:59 -0600

    Add error catch for twilio message load

--- app/helpers/microposts_helper.rb
+++ app/helpers/microposts_helper.rb
@@ -1,14 +1,22 @@
 module MicropostsHelper
   
   def messagesSent
-    client = Twilio::REST::Client.new(ENV['TWILIO_SID'], ENV['TWILIO_TOKEN']) 
-    list = client.account.messages.list({:from => ENV['TWILIO_FROM'], :page_size => 200})
-    listtemp = list
+    begin
+        client = Twilio::REST::Client.new(ENV['TWILIO_SID'], ENV['TWILIO_TOKEN']) 
+        list = client.account.messages.list({:from => ENV['TWILIO_FROM'], :page_size => 200})
+        listtemp = list
+    rescue
+        list = [{:sid => "Error loading messages."}]
+    end
   end
 
   def messagesReceived
-    client = Twilio::REST::Client.new(ENV['TWILIO_SID'], ENV['TWILIO_TOKEN']) 
-    listreceived = client.account.messages.list({:to => ENV['TWILIO_FROM'], :page_size => 200})
+    begin
+        client = Twilio::REST::Client.new(ENV['TWILIO_SID'], ENV['TWILIO_TOKEN']) 
+        listreceived = client.account.messages.list({:to => ENV['TWILIO_FROM'], :page_size => 200})
+    rescue
+        list = [{:sid => "Error loading messages."}]
+    end
   end
   
 end
