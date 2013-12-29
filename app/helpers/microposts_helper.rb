4111c0e3d8a2ef99965356c9ef38e92235461ab4
Josh Kalov <jkalov@gmail.com>
2013-12-29 13:17:21 -0600

    Add better error catch for twilio message retrieve

--- app/helpers/microposts_helper.rb
+++ app/helpers/microposts_helper.rb
@@ -4,9 +4,9 @@ module MicropostsHelper
     begin
         client = Twilio::REST::Client.new(ENV['TWILIO_SID'], ENV['TWILIO_TOKEN']) 
         list = client.account.messages.list({:from => ENV['TWILIO_FROM'], :page_size => 200})
-        listtemp = list
     rescue
-        list = [{:sid => "Error loading messages."}]
+        flash[:error] =  "There was a problem loading sent messages from Twilio."
+        list = []   
     end
   end
 
@@ -15,7 +15,8 @@ module MicropostsHelper
         client = Twilio::REST::Client.new(ENV['TWILIO_SID'], ENV['TWILIO_TOKEN']) 
         listreceived = client.account.messages.list({:to => ENV['TWILIO_FROM'], :page_size => 200})
     rescue
-        list = [{:sid => "Error loading messages."}]
+        flash[:error] =  "There was a problem loading received messages from Twilio."   
+        list = []
     end
   end
   
